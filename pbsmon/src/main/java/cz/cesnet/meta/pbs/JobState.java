5f84766f4ec35856da3eeb8a75205efbb0ac655f
Martin Kuba <makub@ics.muni.cz>
2017-03-20 17:53:01 +0100

    added new job states B, M, U

--- pbsmon/src/main/java/cz/cesnet/meta/pbs/JobState.java
+++ pbsmon/src/main/java/cz/cesnet/meta/pbs/JobState.java
@@ -1,9 +1,24 @@
 package cz.cesnet.meta.pbs;
 
 /**
-* Enum of job states to provide sorting order..
-*
-* @author Martin Kuba makub@ics.muni.cz
-*/
-public enum JobState { Q, R, E, C, W, H ,T, S, X, F }
+ * Enum of job states to provide sorting order.
+ * <p>
+ * B Array job: at least one subjob has started.
+ * E Job is exiting after having run.
+ * F Job is finished.
+ * H Job is held.
+ * M Job was moved to another server.
+ * Q Job is queued.
+ * R Job is running.
+ * S Job is suspended.
+ * T Job is being moved to new location.
+ * U Cycle-harvesting job is suspended due to keyboard activity.
+ * W Job is waiting for its submitter-assigned start time to be reached.
+ * X Subjob has completed execution or has been deleted.
+ *
+ * @author Martin Kuba makub@ics.muni.cz
+ */
+public enum JobState {
+    Q, R, E, C, W, H, T, S, X, F, B, M, U
+}
 
