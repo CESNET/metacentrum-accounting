17c08ff6592ceba249f428f3e06372545333eef8
Martin Kuba <makub@ics.muni.cz>
2017-10-02 11:13:43 +0200

    refactor calling PBS

---
+++ pbsmon/src/main/c/pbsprocaller.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pbs_error.h>
+#include <pbs_ifl.h>
+#include <unistd.h>
+#include <signal.h>
+
+extern char *pbs_server;
+
+int main(int argc, char **argv) {
+
+    printf("hello world\n");
+}
